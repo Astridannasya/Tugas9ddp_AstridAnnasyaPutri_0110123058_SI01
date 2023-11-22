7c7495381b7b7fac2212b448a86b148d6c9ce540
Astridannasya <146051003+Astridannasya@users.noreply.github.com>
2023-11-22 15:21:52 +0700

    Add files via upload

---
+++ No1.py
@@ -0,0 +1,9 @@
+def profil(nama, alamat, gender, umur, hoby):
+    print("Nama :", nama)
+    print("Alamat :", alamat)
+    print("Gender :", gender)
+    print("Umur :", umur)
+    print("hoby :", hoby)
+
+#panggil
+profil("Astrid", "Bogor", "Perempuan", "19 th", "Menonton Badminton")
---
+++ No3.py
@@ -0,0 +1,6 @@
+def ganjil(x):
+    for a in range(1, x+1, 2):
+        print(a)
+
+#pemanggilan
+ganjil(100)
